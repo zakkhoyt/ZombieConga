75e8e74ff981764a204a726738b4456b3de52f7d
Zakk Hoyt <zacharyhoyt@zakkimac.local>
2014-06-14 16:45:52 -0700

    Initial Commit

---
+++ ZombieConga/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZombieConga
+//
+//  Created by Zakk Hoyt on 6/14/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZombieConga/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ZombieConga
+//
+//  Created by Zakk Hoyt on 6/14/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
