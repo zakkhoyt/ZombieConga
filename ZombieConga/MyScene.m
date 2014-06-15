eba1f861aea417b58d49724a243f9b65eae58b41
Zakk Hoyt <zacharyhoyt@zakkimac.local>
2014-06-14 17:07:15 -0700

    first commit

---
+++ ZombieConga/MyScene.m
@@ -0,0 +1,13 @@
+//
+//  MyScene.m
+//  ZombieConga
+//
+//  Created by Zakk Hoyt on 6/14/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import "MyScene.h"
+
+@implementation MyScene
+
+@end
